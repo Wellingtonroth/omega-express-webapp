82f7b7f45c411581cafb4d588c1f56739798c25a
Wellington Roth <wellingtonrothh@gmail.com>
2025-05-12 17:04:08 -0300

    feat:

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,22 +1,26 @@
 <template>
-  <section class="counters">
-    <div class="counter-wrapper">
-      <div>Planos Nutricionais</div>
-      <span>{{ counterPlans }}</span>
+  <section class="counter-section">
+    <div class="counters">
+      <div class="counter-wrapper">
+        <div class="counter-title">Planos Nutricionais</div>
+        <span class="counter-value">+{{ counterPlans }}</span>
+      </div>
+      <div class="counter-wrapper">
+        <div class="counter-title">Clientes Atendidos</div>
+        <span class="counter-value">+{{ counterClients }}</span>
+      </div>
+      <div class="counter-wrapper">
+        <div class="counter-title">Transformações</div>
+        <span class="counter-value">+{{ counterTransformations }}</span>
+      </div>
     </div>
-    <div class="counter-wrapper">
-      <div>Clientes Atendidos</div>
-      <span>{{ counterClients }}</span>
-    </div>
-    <div class="counter-wrapper">
-      <div>Transformações</div>
-      <span>{{ counterTransformations }}</span>
-    </div>
+    <CTAButton :text="'Comece a Consultoria Agora!'" />
   </section>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import CTAButton from '../shared/CTAButton.vue';
 
 const counterPlans = ref(1000);
 const counterClients = ref(1000);
@@ -24,23 +28,40 @@ const counterTransformations = ref(1000);
 </script>
 
 <style lang="scss" scoped>
-.counters {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 20px;
-  background-color: #47403d;
+.counter-section {
+  border: 2px solid #47403d;
   padding: 20px;
-  color: #fff;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
 
-  .counter-wrapper {
-    background-color: rgb(197 197 197 / 34%);
-    padding: 15px 15px 10px 15px;
-    border-radius: 4px 4px 4px 4px;
+  .counters {
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    gap: 20px;
+    margin-bottom: 30px;
+
+    .counter-wrapper {
+      padding: 15px 15px 10px 15px;
+      border-radius: 4px 4px 4px 4px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      border: 2px solid #47403d;
+      width: 244px;
+
+
+      .counter-title {
+        font-size: 20px;
+      }
+
+      .counter-value {
+        font-size: 32px;
+        font-weight: 600;
+      }
+    }
   }
 }
+
 </style>
